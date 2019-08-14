0daf62a33e7c92750a530e57990f64932db2d551
oberljn <oberljn@micro.us-east1-b.c.user0112358d.internal>
2019-08-14 02:03:04 +0000

    Got IO on explorefeatures.py, created README.md.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data Project Tools
+
+## File Manifest
+
+### explorefeatures.py
+Run in new dataset.csv and writes datasetfeatures.csv as an overview of set's columns.
+
+### dtools.py
+Currently includes a drop function that archives dropped series into a JSON file during data cleaning processes.
+
+## To Do
+- Tidy up explorefeatures.py and merge into dtools.py.
--- dtools.py
+++ dtools.py
@@ -23,57 +23,6 @@ def capdropped():
         f.write(capdropped)
         print('dropped.json finalized\n')
 
-def explore(filen):
-    '''
-    writes various data overview csvs
-    filen:  eg, 'data.csv
-    '''
-    # read in
-    df = pd.read_csv(filen)
-    columns = df.columns
-    
-    explore = {
-                'column': [],
-                'datatype': [],
-                'notnan': [],
-                'isnan': [],
-                'unique': [],
-                'mean': [],
-                'mode[0]': [],
-                'median': [],
-                'stdev': [],
-              } 
-
-    # basic info
-    for col in df.columns:
-        explore['column'].append(col)
-        explore['datatype'].append(df[col].dtype)
-        explore['notnan'].append(df[col].count())
-        explore['isnan'].append(df[col].isna().sum())
-        explore['unique'].append(df[col].nunique())
-
-        # stats for non-objects (non-str)
-        if df[col].dtype != 'object':
-            explore['mean'].append(df[col].mean().round(decimals=2))
-            explore['mode[0]'].append(df[col].mode()[0].round(decimals=2)) # mode returns a list of values (two values could occur equally most)
-            explore['median'].append(df[col].median().round(decimals=2))
-            explore['stdev'].append(df[col].std().round(decimals=2))
-
-        # stats=None for objects, except for mode. break out categorical data into new csvs?
-        else:
-            explore['mean'].append(None)
-            explore['mode[0]'].append(df[col].mode()[0]) # mode returns a list of values (two values could occur equally most)
-            explore['median'].append(None)
-            explore['stdev'].append(None)
-    
-    df_explore = pd.DataFrame(data=explore)
-    df_explore.to_csv('explore.csv', index=False)
-    #return(df_explore)
-
-explore('data.csv')
-
-
-
 
 def testout(df, kind, max_rows):
     '''
