b328933b7cb51345020d04ead898274d06115d03
oberljn <joberlin@RST-LTJOBERLIN.acr.org>
2019-10-16 17:04:08 -0400

    Updated ref

--- tools.py
+++ tools.py
@@ -195,9 +195,45 @@ class thumb(object):
             sleep(4)
 
 
+
+def split_expand_row(df, column, sep=',', keep=False):
+    """
+    Split the values of a column and expand so the new DataFrame has one split
+    value per row. Filters out rows where the column is missing.
+
+    Params
+    ------
+    df : pandas.DataFrame
+        dataframe with the column to split and expand
+    column : str
+        the column to split and expand
+    sep : str
+        the string used to split the column's values
+    keep : bool
+        whether to retain the presplit value as it's own row
+
+    Returns
+    -------
+    pandas.DataFrame
+        Returns a dataframe with the same columns as 'df'.
+    """
+    indexes, new_values = list(), list()
+    df = df.dropna(subset=[column])
+    for i, presplit in enumerate(df[column].astype(str)):
+        values = presplit.split(sep)
+        if keep and len(values) > 1:
+            indexes.append(i)
+            new_values.append(presplit)
+        for value in values:
+            indexes.append(i)
+            new_values.append(value)
+    new_df = df.iloc[indexes, :].copy()
+    new_df[column] = new_values
+    return new_df
+
+################################################################################
 testing=False
 if testing == True:
     df=pd.read_csv('test_sets/asa_shooter.csv')
     thumb=thumb(df)
     thumb.through()
-
